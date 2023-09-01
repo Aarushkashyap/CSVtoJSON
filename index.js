fb35f983358a0e63846235a00b50b30028f5dabf
Aarush Kashyap <aarush.kashyap21@gmail.com>
2023-09-01 15:32:05 +0530

    Created sample csv input file, generated converted json file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("New project");
+const { log } = require('console');
+const csvtojson = require('csvtojson');
+const fs = require('fs');
+
+
+const csvfilepath = "./inputCSV/simple.csv"
+
+csvtojson()
+.fromFile(csvfilepath)
+.then((json) => {
+    console.log(json);
+    fs.writeFileSync("./outputJson/output.json",JSON.stringify(json),"utf-8", (err) =>{
+        if(err) console.log(err);
+    })
+})
